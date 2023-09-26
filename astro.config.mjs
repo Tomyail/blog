dae78ec41bc05385f7dfc94b1236ff0e102f6580
Leo sun <tomyail1989@gmail.com>
2023-09-27 00:14:09 +0800

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,15 @@
-import { defineConfig, squooshImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import partytown from "@astrojs/partytown";
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://blog.tomyail.com/',
-  integrations: [
-    mdx(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      }
-    })
-  ],
-  /* image: { */
-  /*   //not support gif */
-  /*   service: squooshImageService(), */
-  /* }, */
+  integrations: [mdx(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), tailwind()]
 });
