82d1f0638f7faae4bf21c1fc3aecc3c9e4fd4cfb
lixuexin03 <lixuexin03@meituan.com>
2023-09-27 17:12:20 +0800

    response style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://blog.tomyail.com/',
-  integrations: [mdx(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), tailwind()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+    tailwind(),
+  ],
 });
